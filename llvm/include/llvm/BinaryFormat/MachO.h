191f6dd8556cd2e1aef81c97da7dca0307400511
Alessandro Di Federico <ale@rev.ng>
2019-05-21 23:13:13 +0200

    MachO: improve relocation information

--- llvm/include/llvm/BinaryFormat/MachO.h
+++ llvm/include/llvm/BinaryFormat/MachO.h
@@ -232,6 +232,7 @@ enum DataRegionType {
 };
 
 enum RebaseType {
+  REBASE_TYPE_INVALID = 0u,
   REBASE_TYPE_POINTER = 1u,
   REBASE_TYPE_TEXT_ABSOLUTE32 = 2u,
   REBASE_TYPE_TEXT_PCREL32 = 3u
@@ -252,6 +253,7 @@ enum RebaseOpcode {
 };
 
 enum BindType {
+  BIND_TYPE_INVALID = 0u,
   BIND_TYPE_POINTER = 1u,
   BIND_TYPE_TEXT_ABSOLUTE32 = 2u,
   BIND_TYPE_TEXT_PCREL32 = 3u
