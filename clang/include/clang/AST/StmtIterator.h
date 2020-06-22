de4b5aaed5de33806e9029d5cd0da35032ee0e9d
Alain Carlucci <alain@rev.ng>
2020-06-22 16:40:11 +0200

    [clang] Fix ambiguous `!=` for `clang::StmtIterator`

--- clang/include/clang/AST/StmtIterator.h
+++ clang/include/clang/AST/StmtIterator.h
@@ -129,6 +129,14 @@ struct StmtIterator : public StmtIteratorImpl<StmtIterator, Stmt*&> {
   StmtIterator(const VariableArrayType *t)
       : StmtIteratorImpl<StmtIterator, Stmt*&>(t) {}
 
+  bool operator==(const StmtIterator& RHS) const {
+    return StmtIteratorImpl::operator==(RHS);
+  }
+
+  bool operator!=(const StmtIterator& RHS) const {
+    return StmtIteratorImpl::operator!=(RHS);
+  }
+
 private:
   StmtIterator(const StmtIteratorBase &RHS)
       : StmtIteratorImpl<StmtIterator, Stmt *&>(RHS) {}
