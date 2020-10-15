7b8fe3e75e61ce5dcc29e3676b033ac91a3b7512
Alain Carlucci <alain@rev.ng>
2020-10-15 16:04:32 +0200

    Triple.cpp: fix getArchTypeForLLVMName for "s390x"

--- llvm/lib/Support/Triple.cpp
+++ llvm/lib/Support/Triple.cpp
@@ -301,6 +301,7 @@ Triple::ArchType Triple::getArchTypeForLLVMName(StringRef Name) {
     .Case("sparcel", sparcel)
     .Case("sparcv9", sparcv9)
     .Case("systemz", systemz)
+    .Case("s390x", systemz)
     .Case("tce", tce)
     .Case("tcele", tcele)
     .Case("thumb", thumb)
