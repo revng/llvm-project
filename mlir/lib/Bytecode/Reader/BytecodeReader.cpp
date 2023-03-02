b93d654bc9267ca2128528583558db618ff9cc1d
Rahul Kayaith <rkayaith@gmail.com>
2023-03-02 16:06:44 -0500

    [mlir][Parser] Make parse{Attribute,Type} null-terminate input
    `parseAttribute` and `parseType` require null-terminated strings as
    input, but this isn't great considering the argument type is
    `StringRef`. This changes them to copy to a null-terminated buffer by
    default, with a `isKnownNullTerminated` flag added to disable the
    copying.
    
    closes #58964
    
    Reviewed By: rriddle, kuhar, lattner
    
    Differential Revision: https://reviews.llvm.org/D145182
    
    (cherry picked from commit 55cf53fd0f5594eb701b5760729fdc2bd4a70584)

--- mlir/lib/Bytecode/Reader/BytecodeReader.cpp
+++ mlir/lib/Bytecode/Reader/BytecodeReader.cpp
@@ -1037,9 +1037,11 @@ LogicalResult AttrTypeReader::parseAsmEntry(T &result, EncodingReader &reader,
   size_t numRead = 0;
   MLIRContext *context = fileLoc->getContext();
   if constexpr (std::is_same_v<T, Type>)
-    result = ::parseType(asmStr, context, &numRead);
+    result =
+        ::parseType(asmStr, context, &numRead, /*isKnownNullTerminated=*/true);
   else
-    result = ::parseAttribute(asmStr, context, Type(), &numRead);
+    result = ::parseAttribute(asmStr, context, Type(), &numRead,
+                              /*isKnownNullTerminated=*/true);
   if (!result)
     return failure();
 
